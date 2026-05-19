9af7ad06773d167d3d5d00ce4cb173bcef388ae3
Gabriel Gonzalez <gavoomz@gmail.com>
2026-05-19 02:17:27 +0100

    Estructura base de App y estilos de CSS.

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from 'react'
+import { supabase } from './supabaseClient'
+import FormularioSubida from './components/FormularioSubida'
+
+function App() {
+  // --- CEREBRO / BACKEND (No tocar) ---
+  const [mods, setMods] = useState([])
+  const [filtroLoader, setFiltroLoader] = useState('Todos')
+  const [filtroVersion, setFiltroVersion] = useState('Todas')
+
+  const obtenerMods = async () => {
+    let query = supabase.from('mods').select('*')
+    if (filtroLoader !== 'Todos') query = query.eq('loader', filtroLoader)
+    if (filtroVersion !== 'Todas') query = query.eq('version', filtroVersion)
+    const { data, error } = await query
+    if (!error) setMods(data)
+  }
+
+  useEffect(() => {
+    obtenerMods()
+  }, [filtroLoader, filtroVersion])
+  // ------------------------------------
+
+  // --- TU HTML (Lienzo en blanco) ---
+  return (
+    <div className="contenedor">
+
+      <p id="linea1"></p>
+      <h1 id="titulo">¡Biblioteca Personal de Mods de Minecraft!</h1>
+      <p id="intro">Esto es un proyecto de prueba, 
+        en el cual sirve de repositorio para subir y descargar mods de Minecraft 
+        de manera ordenada y sencilla</p>
+
+        <div className="img-principal">
+          <img src="/image-10.jpg" className="mc_img" alt="imagen_1" />
+          <img src="/Steve__2.png" className="mc_img" alt="imagen_2" />
+          <img src="/1c3a9f59c02fe16c5a439fb5972dad3e.jpg" className="mc_img" alt="imagen_3" />
+        </div>
+
+      <section className="zonaFormulario">
+        <FormularioSubida />
+      </section>
+
+      <p id="linea2"></p>
+    </div>
+  )
+}
+export default App
